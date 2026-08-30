424d29aa635d781c01f913bfa3ac869c9f969ee7
SifatHazard <iamkaosar16@gmail.com>
2026-08-31 02:22:11 +0600

    Add access-denied

---
+++ access_denied.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = "Access Denied";
+require_once __DIR__ . '/includes/header.php';
+?>
+<div class="empty-state">
+    <h2>🚫 Access Denied</h2>
+    <p>You don't have permission to view this page.</p>
+    <a href="/ecommerce/index.php" class="btn btn-primary mt-20">Go Home</a>
+</div>
+<?php require_once __DIR__ . '/includes/footer.php'; ?>
